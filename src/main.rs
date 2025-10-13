d0148ea8dcc39a0a5223444ed9ec718a36f25d83
Isaac Peterson <isaacpeterson2017@gmail.com>
2025-10-12 22:05:49 -0500

    webasm build *works!
    *it has problems, and is not usable yet, but progress is progress,
    brother. need to increase the window size so you can actually see it and
    then maybe scale it up somewhat.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,38 @@ mod foreign;
 mod slopestake;
 mod ui;
 
+#[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result {
-    ui::SlopeStakerApp::new().launch()
+    ui::SlopeStakerApp::new().launch_native()
+}
+
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    use eframe::wasm_bindgen::JsCast as _;
+
+    // Redirect `log` message to `console.log` and friends:
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        let document = web_sys::window()
+            .expect("No window")
+            .document()
+            .expect("No document");
+
+        let canvas = document
+            .get_element_by_id("the_canvas_id")
+            .expect("Failed to find the_canvas_id")
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .expect("the_canvas_id was not a HtmlCanvasElement");
+
+        let _start_result = eframe::WebRunner::new()
+            .start(
+                canvas,
+                web_options,
+                Box::new(|_cc| Ok(Box::new(ui::SlopeStakerApp::new().launch_web()))),
+            )
+            .await;
+    });
 }
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -13,7 +13,42 @@ impl SlopeStakerApp {
     pub fn new() -> Self {
         SlopeStakerApp::default()
     }
-    pub fn launch(&self) -> eframe::Result {
+    #[cfg(target_arch = "wasm32")]
+        pub fn launch_web(self) -> Self{
+            
+        use eframe::wasm_bindgen::JsCast as _;
+
+        // Redirect `log` message to `console.log` and friends:
+        eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+        let web_options = eframe::WebOptions::default();
+
+        wasm_bindgen_futures::spawn_local(async {
+            let document = web_sys::window()
+                .expect("No window")
+                .document()
+                .expect("No document");
+
+            let canvas = document
+                .get_element_by_id("the_canvas_id")
+                .expect("Failed to find the_canvas_id")
+                .dyn_into::<web_sys::HtmlCanvasElement>()
+                .expect("the_canvas_id was not a HtmlCanvasElement");
+
+            
+            let _start_result = eframe::WebRunner::new()
+                .start(
+                    canvas,
+                    web_options,
+                    Box::new(|_cc| Ok(Box::new(SlopeStakerApp::default()))),
+                )
+                .await;
+
+        });
+        self
+    }
+#[cfg(not(target_arch = "wasm32"))]
+    pub fn launch_native(&self) -> eframe::Result {
         use eframe::AppCreator;
 
         let title = "slope-staker";
